8d600eea979111b59bad7356a98f787fac220c1e
DESKTOP-9MSL9DK\Administrator <smb@softmmp.com>
2020-07-08 13:48:33 +0800

    '解决一些问题'

---
+++ src/Api/Oauth.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace ByteDance\Douyin\Api;
+class Oauth
+{
+    public function __construct($config)
+    {
+
+    }
+
+    public function connect()
+    {
+        echo 'Hello';
+    }
+}
--- src/app.php
+++ src/app.php
@@ -3,8 +3,19 @@
 
 namespace ByteDance\Douyin;
 
-
-class app
+class App
 {
+    public static function make($name , array $config)
+    {
+        $name = ucfirst(strtolower($name));
+        $application = "\\ByteDance\\Douyin\\Api\\{$name}";
 
+        return new $application($config);
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        // TODO: Implement __callStatic() method.
+        return self::make($name , ...$arguments);
+    }
 }
